aaf73b973bffe690d941160db969cac402e8b9d2
Abhishek140304 <kumarsagar2076@gmail.com>
2025-04-10 16:12:35 +0530

    Add files via upload

---
+++ initUsers.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+
+
+addgroup g_user
+addgroup g_admin
+addgroup g_mod
+addgroup g_author
+
+
+CONFIG_FILE="/scripts/sysad-1-users.yaml"
+
+createUser() {
+	 username=$1
+	 group=$2
+	 baseDir=$3
+	 userHome="${baseDir}/${username}"
+
+
+	 if id "$username" &>/dev/null; then	
+	 	echo "User $username already exists."
+	 	
+	 	
+	 else 
+	 	useradd -m -d "$userHome" -g "$group" "$username"
+	 	echo "Created user: $username group: $group with home: $userHome"
+	 
+	 fi
+
+}
+
+
+
+parseUsers(){
+										
+	role=$1
+        group=$2
+        baseDir=$3
+	
+	usernames=$(awk -v role="$role" '
+		  $0 ~ "^" role ":" { in_role=1; next }
+  		  /^[a-z]+:/ { in_role=0 }
+  		  in_role && $1 ~ /username:/ {
+    		  print $2}
+		  ' "$CONFIG_FILE")
+
+
+
+    	for user in $usernames; do
+        	createUser "$user" "$group" "$baseDir"
+        	usermod -a -G $group $user
+    	done
+}
+
+
+
+parseUsers "admins" "g_admin" "/home/admin"
+parseUsers "authors" "g_author" "/home/authors"
+parseUsers "users" "g_user" "/home/users"
+parseUsers "mods" "g_mod" "/home/mods"
