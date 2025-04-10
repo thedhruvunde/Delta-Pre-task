c2506216eefd39b4c98862f3cf8f7c65ed77731d
Dhruv Unde <dhruvunde@gmail.com>
2025-04-10 22:39:28 +0530

    Update initUsers.sh

--- initUsers.sh
+++ initUsers.sh
@@ -12,7 +12,6 @@ CONFIG_FILE="/scripts/sysad-1-users.yaml"
 
 createUser() {
 	 username=$1
-	 group=$2
 	 baseDir=$3
 	 userHome="${baseDir}/${username}"
 
@@ -22,7 +21,7 @@ createUser() {
 	 	
 	 	
 	 else 
-	 	useradd -m -d "$userHome" -g "$group" "$username"
+	 	useradd -m -d "$userHome" "$username"
 	 	echo "Created user: $username group: $group with home: $userHome"
 	 
 	 fi
@@ -35,8 +34,8 @@ subDirs=("blogs" "public")
 parseUsers(){
 										
 	role=$1
-        group=$2
-        baseDir=$3
+    group=$2
+    baseDir=$3
 	
 	usernames=$(awk -v role="$role" '
 		  $0 ~ "^" role ":" { in_role=1; next }
@@ -44,18 +43,16 @@ parseUsers(){
   		  in_role && $1 ~ /username:/ {
     		  print $2}
 		  ' "$CONFIG_FILE")
-
-
-	if [ "$group" = "g_author" ]; then
-    	for dir in "${subDirs[@]}"; do
-    		mkdir -p "/home/authors/$username/$dir"
-    	done
-    fi
 	
 	
     	for user in $usernames; do
-        	createUser "$user" "$group" "$baseDir"
+        	createUser "$user" "$baseDir"
         	usermod -a -G $group $user
+            if [ "$group" = "g_author" ]; then
+    	        for dir in "${subDirs[@]}"; do
+    		        mkdir -p "/home/authors/$username/$dir"
+    	        done
+            fi
     	done
 }
 
