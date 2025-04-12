771b34da1d043071c3db05c85eaf9c4323328a5d
Abhishek140304 <kumarsagar2076@gmail.com>
2025-04-12 17:47:02 +0530

    Update initUsersakfjogo.sh

--- initUsersakfjogo.sh
+++ initUsersakfjogo.sh
@@ -1,118 +1,129 @@
 #!/bin/bash
 
+
+
 addgroup g_user
 addgroup g_admin
 addgroup g_mod
 addgroup g_author
 
+
 CONFIG_FILE="/scripts/sysad-1-users.yaml"
 
 createUser() {
-	username=$1
-	userHome=$2
-	group=$3
-	role=$4
+	 username=$1
+	 userHome=$2
+	 group=$3
+     role=$4
 
-	if id "$username" &>/dev/null; then
-		echo "User $username already exists."
-	else
-		useradd -m -d "$userHome" -s /bin/bash "$username"
-		mkdir -p "$userHome"
-		usermod -a -G "$group" "$username"
-		chown "$username:$group" "$userHome"
-		echo "Created user: $username group: $group with home: $userHome"
-	fi
+
+	 if id "$username" &>/dev/null; then	
+	 	echo "User $username already exists."
+		 	
+    else 
+	 	useradd -m -d $userHome $username
+        usermod -a -G $group $username
+	 	echo "Created user: $username group: $group with home: $userHome" 
+	 fi
+
 }
+
+
 
 parseUsers(){
+										
 	role=$1
+	
 	awk -v role="$role" '
 	$0 ~ "^" role ":" { in_role=1; next }
-	/^[a-z]+:/ { in_role=0 }
-	in_role && $1 ~ /username:/ {
-	print $2 }' "$CONFIG_FILE"
+  	/^[a-z]+:/ { in_role=0 }
+  	in_role && $1 ~ /username:/ {
+    print $2} ' "$CONFIG_FILE"
+	
+           
 }
 
-# --- 1. Create users ---
+echo "Processing admins..."
 parseUsers "admins" | while read -r user; do
-	HomeDir="/home/admin/$user"
+	HomeDir=/home/admin/$user
 	createUser "$user" "$HomeDir" "g_admin" "admin"
 done
 
+echo "Processing users..."
 parseUsers "users" | while read -r user; do
-	HomeDir="/home/users/$user"
+	HomeDir=/home/users/$user
 	createUser "$user" "$HomeDir" "g_user" "users"
 done
 
+echo "Processing authors..."
 parseUsers "authors" | while read -r user; do
-	HomeDir="/home/authors/$user"
+	HomeDir=/home/authors/$user
 	createUser "$user" "$HomeDir" "g_author" "author"
-
+	
 	subDirs=("blogs" "public")
 	for dir in "${subDirs[@]}"; do
-		mkdir -p "$HomeDir/$dir"
-		chown "$user:g_author" "$HomeDir/$dir"
+        	mkdir -p "/home/authors/$user/$dir"
+        	echo "Author $user directories created" 
 	done
-	echo "Author $user directories created"
 done
 
+
+echo "Processing mods..."
 parseUsers "mods" | while read -r user; do
-	HomeDir="/home/mods/$user"
+	HomeDir=/home/mods/$user
 	createUser "$user" "$HomeDir" "g_mod" "mods"
 done
 
-# --- 2. Directory Permissions ---
-# Restrict access: only user/author can access their own home directory
-chmod -R o-rwx /home/users
-chmod -R o-rwx /home/authors
 
-for user in $(parseUsers "users"); do
-	chmod 700 "/home/users/$user"
+echo "Configuring user permissions..."
+parseUsers "users" | while read -r user; do
+    chmod 700 /home/users/$user
 done
 
-for user in $(parseUsers "authors"); do
-	chmod 700 "/home/authors/$user"
-	chmod 755 "/home/authors/$user/public"  # public should be readable
+echo "Configuring admin permissions..."
+parseUsers "admins" | while read -r user; do
+    chmod 700 /home/admin/$user
+    usermod -a -G sudo $user
 done
 
-# Allow admins full access
-chgrp g_admin /home/users /home/authors /home/mods
-chmod -R g+rwx /home/users /home/authors /home/mods
 
-# --- 3. Mod-to-author public access ---
-parseModAuthorMapping() {
-	awk '
-	$0 ~ "^mods_to_authors:" { in_map=1; next }
-	/^[a-z]+:/ { in_map=0 }
-	in_map && /mod:/ {
-		mod=$2
-		getline; gsub("author: ", "", $0)
-		author=$0
-		print mod, author
-	}' "$CONFIG_FILE"
+parseMods() {
+    mod_username=$1
+
+    awk -v target="$mod_username" '
+    $0 ~ /mods:/ { in_mods = 1; next }
+    in_mods && /^[a-z]+:/ { in_mods = 0 }  # exit mods section
+
+    in_mods && $0 ~ /username:[[:space:]]*"'$mod_username'"[[:space:]]*$/ {
+        in_target = 1
+        next
+    }
+
+    in_target && $0 ~ /authors:/ {
+        in_authors = 1
+        next
+    }
+
+    in_authors && $1 ~ /^-$/ {
+        gsub("-", "", $1)
+        gsub(/[[:space:]]*/, "", $0)
+        print $0
+        next
+    }
+
+    in_authors && $0 !~ /^- / {
+        in_authors = 0
+        in_target = 0
+    }
+    ' "$CONFIG_FILE"
 }
 
-parseModAuthorMapping | while read -r mod author; do
-	modHome="/home/mods/$mod"
-	pubDir="/home/authors/$author/public"
-	if [ -d "$pubDir" ]; then
-		setfacl -m u:"$mod":rwX "$pubDir"
-		echo "Granted $mod RW access to $author's public dir"
-	fi
+
+echo "Configuring mods permissions..."
+parseUsers "mods" | while read -r user; do
+    chmod 700 /home/mods/$user
+    parseMods "mods" "user" | while read -r authorname; do
+    usermod -aG $authorname $user
+done
 done
 
-# --- 4. Create all_blogs with symlinks for users ---
-mkdir -p /home/all_blogs
-chown root:root /home/all_blogs
-chmod 755 /home/all_blogs
-
-for user in $(parseUsers "users"); do
-	userAllBlogs="/home/users/$user/all_blogs"
-	mkdir -p "$userAllBlogs"
-	chown "$user:g_user" "$userAllBlogs"
-	chmod 555 "$userAllBlogs"
-
-	for author in $(parseUsers "authors"); do
-		ln -sf "/home/authors/$author/public" "$userAllBlogs/$author"
-	done
-done
