0497ca0d408539cbf676cc05b3ff019b37574b2e
Dhruv Unde <dhruvunde@gmail.com>
2025-04-13 00:29:11 +0530

    Update ts.sh

--- ts.sh
+++ ts.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/local/bin/yq
+sudo chmod +x /usr/local/bin/yq
 yq '.mods[] | select(.username == "'"$1"'") | .authors[]' users.yaml
