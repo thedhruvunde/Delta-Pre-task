57bd571357cdb18acd7ec7f600044d6f0f248808
Abhishek140304 <kumarsagar2076@gmail.com>
2025-05-09 14:28:58 +0530

    Add files via upload

--- manageBlogs.sh
+++ manageBlogs.sh
@@ -6,73 +6,69 @@ BLOGS_DIR="$AUTHOR_HOME/blogs"
 PUBLIC_DIR="$AUTHOR_HOME/public"
 META_FILE="$AUTHOR_HOME/blogs.yaml"
 
-CATEGORIES=("Sports" "Cinema" "Technology" "Travel" "Food" "Lifestyle" "Finance")
+p_flag=""
+a_flag=""
+d_flag=""
+e_flag=""
+CATS=""
 
-touch "$META_FILE"
-yq e 'if .blogs then . else {blogs: []} end' -i "$META_FILE"
-
-get_blog_index() {
-    local filename=$1
-    yq e ".blogs | to_entries | map(select(.value.filename == \"$filename\")) | .[0].key" "$META_FILE"
+get_categories() {
+    yq '.categories[]' "$META_FILE" | sort -u
 }
 
-select_categories() {
-    echo "Select categories (comma-separated index, e.g., 2,1):"
-    for i in "${!CATEGORIES[@]}"; do
-        echo "$i) ${CATEGORIES[$i]}"
-    done
-    read -p "Enter: " input
-    IFS=',' read -ra ORDER <<< "$input"
-    SELECTED=()
-    for i in "${ORDER[@]}"; do
-        SELECTED+=("\"${CATEGORIES[$i]}\"")
-    done
-    echo "[${SELECTED[*]}]"
-}
-
-case "$1" in
-    -p)
-        FILENAME="$2"
-        [ ! -f "$BLOGS_DIR/$FILENAME" ] && echo "File not found in blogs/" && exit 1
-        CATS=$(select_categories)
-        ln -sf "$BLOGS_DIR/$FILENAME" "$PUBLIC_DIR/$FILENAME"
-        chmod 644 "$PUBLIC_DIR/$FILENAME"
-        IDX=$(get_blog_index "$FILENAME")
-        if [[ "$IDX" == "null" ]]; then
-            yq -i ".blogs += [{filename: \"$FILENAME\", status: \"published\", categories: $CATS}]" "$META_FILE"
-        else
-            yq -i ".blogs[$IDX].status = \"published\" | .blogs[$IDX].categories = $CATS" "$META_FILE"
+while getopts "p:a:d:e:" opt; do
+  case $opt in
+    p)
+        p_flag=$OPTARG
+        echo "Enter Categories for the blog: "
+        read CATS
+        
+        if [ ! -f "$BLOGS_DIR/$p_flag" ]; then
+        echo "Error: Blog file '$p_flag' does not exist in $BLOGS_DIR."
+        exit 1
         fi
-        echo "[+] Published $FILENAME"
-        ;;
 
-    -a)
-        FILENAME="$2"
-        [ -f "$PUBLIC_DIR/$FILENAME" ] && rm "$PUBLIC_DIR/$FILENAME"
-        IDX=$(get_blog_index "$FILENAME")
-        [ "$IDX" != "null" ] && yq -i ".blogs[$IDX].status = \"archived\"" "$META_FILE"
-        echo "[+] Archived $FILENAME"
+        yq -i ".blogs += [{\"file_name\": \"$p_flag\", \"publish_status\": true, \"cat_order\": \"$CATS\"}]" "$META_FILE"
+        ln -s "$BLOGS_DIR/$p_flag" "$PUBLIC_DIR/$p_flag"
+        chmod o+r "$BLOGS_DIR/$p_flag"
         ;;
+    a)
+        a_flag=$OPTARG
+        echo "Archiving blog..."
+        rm "$PUBLIC_DIR/$a_flag"
+        bindex=$(yq ".blogs | to_entries | map(select(.value.file_name == \"$a_flag\")) | .[0].key" "$META_FILE")
+        yq -i '.blogs[$bindex].publish_status = false' "$META_FILE"
+        chmod o-r "$BLOGS_DIR/$a_flag"
 
-    -d)
-        FILENAME="$2"
-        rm -f "$BLOGS_DIR/$FILENAME" "$PUBLIC_DIR/$FILENAME"
-        IDX=$(get_blog_index "$FILENAME")
-        [ "$IDX" != "null" ] && yq -i "del(.blogs[$IDX])" "$META_FILE"
-        echo "[+] Deleted $FILENAME"
         ;;
-
-    -e)
-        FILENAME="$2"
-        IDX=$(get_blog_index "$FILENAME")
-        [ "$IDX" == "null" ] && echo "Blog not found." && exit 1
-        CATS=$(select_categories)
-        yq -i ".blogs[$IDX].categories = $CATS" "$META_FILE"
-        echo "[+] Updated categories for $FILENAME"
+    d)
+        d_flag=$OPTARG
+        echo "Deleting blog..."
+        rm -f "$PUBLIC_DIR/$d_flag"
+        rm -f "$BLOGS_DIR/$d_flag"
+        bindex=$(yq ".blogs | to_entries | map(select(.value.file_name == \"$d_flag\")) | .[0].key" "$META_FILE")
+        yq -i 'del(.blogs[$bindex])' "$META_FILE"
         ;;
-
-    *)
-        echo "Usage: $0 -p|-a|-d|-e <filename>"
+    e)
+        e_flag=$OPTARG
+        echo "Enter the order of categories"
+        i=1
+        get_categories | while read -r category; do
+            echo "$i -> $category"
+            ((i++))
+        done
+        read cats
+        bindex=$(yq ".blogs | to_entries | map(select(.value.file_name == \"$e_flag\")) | .[0].key" "$META_FILE")
+        yq -i ".blogs[$bindex].cat_order = \"$cats\"" "$META_FILE"
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
         exit 1
         ;;
-esac
+    :)
+        echo "Option -$OPTARG requires an argument." >&2
+        exit 1
+        ;;
+  esac
+done
+
