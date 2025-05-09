600c5ac65faf9f831b51e527e924e891b1c5e13d
Dhruv Unde <dhruvunde@gmail.com>
2025-05-09 10:31:40 +0530

    Update manageBlogs.sh

--- manageBlogs.sh
+++ manageBlogs.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
+
 p_flag=""
 a_flag=""
 d_flag=""
 e_flag=""
 catl=""
-BLOG_CONFIG="/scripts/blogs.yaml"
+authorid=$USER
+BLOG_CONFIG="/home/authors/$authorid/blogs.yaml"
 
 get_categories() {
-    yq "[].categories" "$BLOG_CONFIG" | sort -u
+    yq '.categories[]' "$BLOG_CONFIG" | sort -u
 }
+
 while getopts "p:a:d:e:" opt; do
   case $opt in
     p)
         p_flag=$OPTARG
-        echo "Enter Categories in which the blog belong: "
+        echo "Enter Categories in which the blog belongs: "
         read catl
-        echo catl
-        ln -s "$p_flag" "~/public/$p_flag"
+        yq -i '.blogs += [{"file_name": "$p_flag", "publish_status": true, "cat_order": $catl}]' blogs.yaml
+        ln -s "$p_flag" "$HOME/public/$p_flag"
         ;;
     a)
         a_flag=$OPTARG
         echo "Archiving blog..."
-        rm "~/public/$a_flag"
+        rm "$HOME/public/$a_flag"
+        bindex="yq '.blogs | to_entries | map(select(.value.file_name == "blog.txt")) | .[0].key' blogs.yaml"
+        yq -i '.blogs[$bindex].publish_status = false' blogs.yaml
         ;;
     d)
         d_flag=$OPTARG
         echo "Deleting blog..."
-        rm "~/public/$d_flag"
-        rm "~/blogs/$d_flag"
+        rm "$HOME/public/$d_flag"
+        rm "$HOME/blogs/$d_flag"
+        bindex="yq '.blogs | to_entries | map(select(.value.file_name == "blog.txt")) | .[0].key' blogs.yaml"
+        yq -i 'del(.blogs[$bindex])' blogs.yaml
         ;;
     e)
         e_flag=$OPTARG
@@ -35,15 +42,19 @@ while getopts "p:a:d:e:" opt; do
         i=1
         get_categories | while read -r category; do
             echo "$i -> $category"
-            i=i+1
+            ((i++))
+        done
+        read cats
+        bindex="yq '.blogs | to_entries | map(select(.value.file_name == "blog.txt")) | .[0].key' blogs.yaml"
+        yq -i '.blogs[$bindex].cat_order = $cats' blogs.yaml
         ;;
     \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
+        echo "Invalid option: -$OPTARG" >&2
+        exit 1
+        ;;
     :)
-      echo "Option -$OPTARG requires an argument." >&2
-      exit 1
-      ;;
+        echo "Option -$OPTARG requires an argument." >&2
+        exit 1
+        ;;
   esac
 done
