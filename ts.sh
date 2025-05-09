fd7312ea2ea0d286e9115aeccf45a6c5b65abe32
Dhruv Unde <dhruvunde@gmail.com>
2025-05-09 10:06:34 +0530

    Update ts.sh

--- ts.sh
+++ ts.sh
@@ -1,4 +1,52 @@
 #!/bin/bash
-sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/local/bin/yq
-sudo chmod +x /usr/local/bin/yq
-yq '.mods[] | select(.username == "'"$1"'") | .authors[]' users.yaml
+
+p_flag=""
+a_flag=""
+d_flag=""
+e_flag=""
+catl=""
+BLOG_CONFIG="/scripts/blogs.yaml"
+
+get_categories() {
+    yq '.categories[]' "$BLOG_CONFIG" | sort -u
+}
+
+while getopts "p:a:d:e:" opt; do
+  case $opt in
+    p)
+        p_flag=$OPTARG
+        echo "Enter Categories in which the blog belongs: "
+        read catl
+        echo "$catl"
+        ln -s "$p_flag" "$HOME/public/$p_flag"
+        ;;
+    a)
+        a_flag=$OPTARG
+        echo "Archiving blog..."
+        rm "$HOME/public/$a_flag"
+        ;;
+    d)
+        d_flag=$OPTARG
+        echo "Deleting blog..."
+        rm "$HOME/public/$d_flag"
+        rm "$HOME/blogs/$d_flag"
+        ;;
+    e)
+        e_flag=$OPTARG
+        echo "Enter the order of categories"
+        i=1
+        get_categories | while read -r category; do
+            echo "$i -> $category"
+            ((i++))
+        done
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        exit 1
+        ;;
+    :)
+        echo "Option -$OPTARG requires an argument." >&2
+        exit 1
+        ;;
+  esac
+done
