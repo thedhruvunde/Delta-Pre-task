3207ade7ade14e5b5a9d143e44b4c39596c7fa4d
Dhruv Unde <dhruvunde@gmail.com>
2025-05-10 11:47:34 +0530

    update

--- blogFilter.sh
+++ blogFilter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+bList="/home/mods/$USER/blacklist.txt"
 while getopts "f:" opt; do
   case $opt in
     f)
@@ -8,6 +8,10 @@ while getopts "f:" opt; do
         bWords=0
         cat $filename | while read -r line; do
             echo "$lineNumber: $line"
+            echo $line | while read -r word; do
+                cat $bList | while read -r bword; do
+                    if [[$word == $bword]]; then
+                        break
             ((lineNumber++))
         done
         ;;
